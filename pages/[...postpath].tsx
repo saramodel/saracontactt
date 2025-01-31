09ea7249ee5db964d98f248fd73d7a26bcea525e
saramodel <kmstasha629@gmail.com>
2025-02-01 00:56:44 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://safesmrt.com/S8AVrL2JTS/` + encodeURI(path as string)
+					`https://safesmrt.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
